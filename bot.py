f78f960426d52c85694388ed42aa5f3aa1c22b62
HAMZAOUI <78570017+imhamzaoui@users.noreply.github.com>
2022-02-23 15:14:31 +0100

    adding txt read

--- bot.py
+++ bot.py
@@ -2,9 +2,9 @@ import requests
 import threading
 url = "https://www.sarhne.com/ajax/messages/send.html"
 id = ""
-def post():
+def post(_msg):
    #msg=this+test+all+opppp&show_my_info=on&img_code=&link=103961398130919630158&bad_words=false&i=null
-    myobj = {'msg': ' this spam bot ',  #this the msg
+    myobj = {'msg': str(_msg),  #this the msg
     'show_my_info':'on',
     'img_code':'',
     'link':str(id),  #id of user
@@ -16,10 +16,10 @@ def post():
   
     print(x.text)
 
-def start(ddos):
+def start(ddos,msg):
     threads = []
     for i in range(ddos):
-        t=threading.Thread(target=post())
+        t=threading.Thread(target=post(msg))
         t.daemon = True 
         threads.append(t)
 
@@ -31,8 +31,19 @@ def start(ddos):
 
 
 id = input('Enter ID : ')
+
+mqn = int(input(" 1) String Message \n 2) Text File <msg.txt> \n>>"))
+
+if (mqn==1):
+    msg = str(input('Enter String msg : '))
+else:
+    print('[+]Reading msg.txt ..')
+    f = open("msg.txt", "r")
+    msg = f.read()
+
 sp = int(input("Enter Spam Number : "))
-start(sp)
+print(f" ID : {id} , SPAM : {sp} , MSG : ({msg}) ")
+start(sp,msg)
 
     
 
